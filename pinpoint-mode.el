8609341ccc7643f4caddc135a5e61fc62297bf68
philanecros <philanecros@gmail.com>
2011-09-01 20:35:24 +0800

    Add command line options.

--- pinpoint-mode.el
+++ pinpoint-mode.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2011 Philanecros Heliostein <philanecros@gmail.com>
 
 ;; Author: Philanecros Heliostein
-;; Keywords: extensions
+;; Keywords: extensions pinpoint
 
 ;;; License:
 
@@ -26,23 +26,75 @@
 (defconst pinpoint-mode-version "0.1"
   "Version of `pinpoint-mode'.")
 
-;; Local variables
+
+;; Local constants and variables
 (defgroup pinpoint nil
-  "Local variables for pinpoint mode."
+  "pinpoint mode."
   :prefix "pinpoint-"
   :group 'languages)
+
+; Command line
+(defcustom pinpoint-executable "pinpoint"
+  "The name of the pinpoint executable."
+  :type 'file
+  :group 'pinpoint)
+
+; Command line options
+(defconst pinpoint-maximized-option "--maximized"
+  "Pinpoint maximized option, maximized without window decoration.")
+(defconst pinpoint-fullscreen-option "--fullscreen"
+  "Pinpoint fullscreen option, fullscreen mode.")
+(defconst pinpoint-speakermode-option "--speakermode"
+  "Pinpoint speakermode option, speakermode window.")
+(defconst pinpoint-rehearse-option "--rehearse"
+  "Pinipoint rehearse option, rehearse timings.")
+
+(defconst pinpoint-output-option "--output="
+  "Pinpoint output option, output to PDF file.")
+(defcustom pinpoint-output-value ""
+  "Name of output file."
+  :type 'string
+  :group 'pinpoint)
+
+(defconst pinpoint-camera-option "--camera="
+  "Pinpoint camera option.")
+(defcustom pinpoint-camera-value ""
+  "Device to use for [camera] background."
+  :type 'string
+  :group 'pinpoint)
+
+(defconst pinpoint-display-option "--display="
+  "Pinpoint display option.")
+(defcustom pinpoint-display-value ""
+  "X display to use."
+  :type 'string
+  :group 'pinpoint)
+
+(defconst pinpoint-screen-option "--screen="
+  "Pinpoint screen option.")
+(defcustom pinpoint-screen-value ""
+  "X screen to use."
+  :type 'string
+  :group 'pinpoint)
+
 
 ;; Keymap
 (defvar pinpoint-mode-map nil
   "Keymap for pinpoint mode.")
 
+
 ;; Mode hook
 (defvar pinpoint-mode-hook nil
   "Mode hook for pinpoint mode.")
 
-;; Sytax table
+
+;; Syntax table
 (defvar pinpoint-mode-syntax-table nil
   "Syntax table for pinpoint mode.")
+
+
+;; Functions
+
 
 
 ;;;###autoload
